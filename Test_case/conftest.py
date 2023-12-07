b2f866e04757ab427ecb8170f48a28196c257f65
Devendra Singh Shakya <davshakya@gmail.com>
2023-12-07 13:16:21 +0530

    code added for removed unused file/dir

--- Test_case/conftest.py
+++ Test_case/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 import os
+import shutil
+
+
 
 
 @pytest.fixture
@@ -15,4 +18,16 @@ def set_environment_variable(request):
 
 # Command-line option to set the environment
 def pytest_addoption(parser):
+    remove_unused_files_dir()
     parser.addoption("--environment", action="store", default="stag", help="Specify the test environment")
+
+
+def remove_unused_files_dir():
+    directory_path = 'D:/Dev_Progs/cyanconnode_api_project/report'
+    cache_file='D:/Dev_Progs/cyanconnode_api_project/.pytest_cache'
+    try:
+        shutil.rmtree(directory_path)
+        shutil.rmtree(cache_file)
+        print(f"The directory {directory_path} has been deleted.")
+    except:
+        print(f"The directory {directory_path} does not exist.")
